b8c821f4b06f903afe0d1facc57f403414801b6b
Eduardo <eduardo_lg98@hotmail.com>
2024-03-06 18:13:50 -0600

    Lesson 3

---
+++ 03_Project1/Project1.py
@@ -0,0 +1,5 @@
+# In this project, I'm asking the user for two questions to give a name for a new brand of beer
+
+print('--------------- Name for a Brand Generator ---------------')
+
+print('The name of the beer is:' + ' \"' + input('What is your favorite TV serie?') + ' ' + input('How are you?') + "\"" )
